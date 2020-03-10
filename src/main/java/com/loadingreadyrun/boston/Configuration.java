85e5123cf7809dc292a5414f8ff8fe6b5f45362e
DarkMorford <DarkMorford@users.noreply.github.com>
2020-03-10 16:07:17 -0700

    Create config class for HTTP listener

--- src/main/java/com/loadingreadyrun/boston/Configuration.java
+++ src/main/java/com/loadingreadyrun/boston/Configuration.java
@@ -1,5 +1,6 @@
 package com.loadingreadyrun.boston;
 
+import com.loadingreadyrun.boston.config.HttpConfig;
 import com.loadingreadyrun.boston.config.TwitchConfig;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,12 +11,14 @@ import net.minecraftforge.fml.config.ModConfig;
 public class Configuration {
     private static ForgeConfigSpec serverConfigSpec;
 
+    public static HttpConfig HTTP;
     public static TwitchConfig TWITCH;
 
     public static ForgeConfigSpec getServerConfigSpec() {
         if (serverConfigSpec == null) {
             ForgeConfigSpec.Builder b = new ForgeConfigSpec.Builder();
             TwitchConfig.setupConfigSpec(b);
+            HttpConfig.setupConfigSpec(b);
             serverConfigSpec = b.build();
         }
         return serverConfigSpec;
@@ -23,6 +26,7 @@ public class Configuration {
 
     @SubscribeEvent
     public static void onLoad(final ModConfig.Loading configEvent) {
+        HTTP = new HttpConfig(configEvent.getConfig().getConfigData());
         TWITCH = new TwitchConfig(configEvent.getConfig().getConfigData());
     }
 
---
+++ src/main/java/com/loadingreadyrun/boston/config/HttpConfig.java
@@ -0,0 +1,41 @@
+package com.loadingreadyrun.boston.config;
+
+import com.electronwill.nightconfig.core.UnmodifiableCommentedConfig;
+import com.electronwill.nightconfig.core.UnmodifiableConfig;
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HttpConfig {
+    public static final String SECTION = "http";
+    private static final Logger LOGGER = LogManager.getLogger();
+    private String listenAddress;
+    private int listenPort;
+
+    public HttpConfig(UnmodifiableCommentedConfig configData) {
+        UnmodifiableConfig cfg = configData.get(SECTION);
+        listenAddress = cfg.get("httpListenAddress");
+        listenPort = cfg.get("httpListenPort");
+    }
+
+    public static void setupConfigSpec(ForgeConfigSpec.Builder builder) {
+        builder.comment("Control panel web server").push(SECTION);
+
+        builder.worldRestart()
+            .comment("IP address to listen for connections on")
+            .define("httpListenAddress", "0.0.0.0");
+
+        builder.worldRestart()
+            .comment("Network port to listen on")
+            .defineInRange("httpListenPort", 38911, 1025, 65535);
+        builder.pop();
+    }
+
+    public String getListenAddress() {
+        return listenAddress;
+    }
+
+    public int getListenPort() {
+        return listenPort;
+    }
+}
